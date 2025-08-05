847e1649a369a9790c0479318e6a842a4b616488
Suraj Mishra <surajmishra88266@gmail.com>
2025-08-05 19:01:22 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const login = require('./routes/login');
+const signup = require('./routes/signup');
 const port = 3000
 
 app.set('view engine', 'ejs')
@@ -9,6 +11,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.use('/login', login);
+app.use('/signup', signup);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
